bd73ab33e5a36cedbecec1a05efae8376f81616a
ovi-tsb <ovi_tsb@yahoo.com>
2023-06-16 19:02:59 -0400

    Changes for heroku first deploy

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module FamilyRecipes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.assets.initialize_on_precompile = false
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
